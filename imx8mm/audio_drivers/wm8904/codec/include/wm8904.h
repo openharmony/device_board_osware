75d98b7b93bd1f632b616de725ef016791d6ee7f
zhaoxc0502 <zhaoxc0502@thundersoft.com>
2022-06-07 01:15:18 +0800

    commit device board osware to sig
    Signed-off-by: zhaoxc0502 <zhaoxc0502@thundersoft.com>

--- imx8mm/audio_drivers/wm8904/codec/include/wm8904.h
+++ imx8mm/audio_drivers/wm8904/codec/include/wm8904.h
@@ -1,10 +1,19 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * wm8904.h  --  WM8904 ASoC driver
+ * Copyright© 2021–2022 Beijing OSWare Technology Co., Ltd
+ * This file contains confidential and proprietary information of
+ * OSWare Technology Co., Ltd
  *
- * Copyright 2009 Wolfson Microelectronics, plc
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Author: Mark Brown <broonie@opensource.wolfsonmicro.com>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef _WM8904_H
